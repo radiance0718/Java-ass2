d0371dd3344840c56bc3f2067934ec578d96b673
radiance0718 <3362985694@qq.com>
2023-04-25 14:31:42 +0800

    dev1.2(with checkstyle)

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
@@ -1,5 +1,7 @@
 package cn.edu.sustech.cs209.chatting.client;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -7,32 +9,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch();
+  public static void main(String[] args) {
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) {
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+      stage.setScene(new Scene(fxmlLoader.load()));
+      stage.setOnHidden(event -> {
+        Controller c = fxmlLoader.getController();
+        c.shutdown();
+        Platform.exit();
+      });
+      stage.setTitle("Chatting Client");
+      stage.show();
+
+    } catch (IOException e) {
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setContentText("Cancle");
+      alert.show();
     }
 
-    @Override
-    public void start(Stage stage){
-        try{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
-            stage.setScene(new Scene(fxmlLoader.load()));
-            stage.setOnHidden(event -> {
-                    Controller c = fxmlLoader.getController();
-                    c.shutdown();
-                    Platform.exit();
-            });
-            stage.setTitle("Chatting Client");
-            stage.show();
-
-        }catch(IOException e){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("Cancle");
-            alert.show();
-        }
-
-    }
+  }
 }
--- chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
+++ chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
@@ -1,51 +1,48 @@
 package cn.edu.sustech.cs209.chatting.common;
 
-import java.util.ArrayList;
-
 public class Message {
 
-    private Long timestamp;
+  private Long timestamp;
 
-    private String sentBy;
+  private String sentBy;
 
-    private String sendTo;
+  private String sendTo;
 
-    private String data;
+  private String data;
 
-    public Message(Long timestamp, String sentBy, String sendTo, String data) {
-        this.timestamp = timestamp;
-        this.sentBy = sentBy;
-        this.sendTo = sendTo;
-        this.data = data;
-    }
+  public Message(Long timestamp, String sentBy, String sendTo, String data) {
+    this.timestamp = timestamp;
+    this.sentBy = sentBy;
+    this.sendTo = sendTo;
+    this.data = data;
+  }
 
+  public static Message getMessage(String s) {
+    String[] li = s.split("/", 4);
+    Message ret = new Message(Long.parseLong(li[0]), li[1], li[2], li[3]);
+    //System.out.println(new Message(timestamp, sentBy, sendTo, data));
+    System.out.println(ret);
+    return ret;
+  }
 
-    public Long getTimestamp() {
-        return timestamp;
-    }
+  public Long getTimestamp() {
+    return timestamp;
+  }
 
-    public String getSentBy() {
-        return sentBy;
-    }
+  public String getSentBy() {
+    return sentBy;
+  }
 
-    public String getSendTo() {
-        return sendTo;
-    }
+  public String getSendTo() {
+    return sendTo;
+  }
 
-    public String getData() {
-        return data;
-    }
+  public String getData() {
+    return data;
+  }
 
-    @Override
-    public String toString(){
-        return this.timestamp.toString()+"/"+this.sentBy+"/"+this.sendTo+"/"+this.data;
-    }
-
-    public static Message getMessage(String s){
-        String[] li = s.split("/", 4);
-        Message ret = new Message(Long.parseLong(li[0]), li[1], li[2], li[3]);
-        //System.out.println(new Message(timestamp, sentBy, sendTo, data));
-        System.out.println(ret);
-        return ret;
-    }
+  @Override
+  public String toString() {
+    return this.timestamp.toString() + "/" + this.sentBy + "/" + this.sendTo + "/" + this.data;
+  }
 }
